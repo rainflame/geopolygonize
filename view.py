d7c7e760ce4b43c5233846b2ea96f997e71adc27
slz4025 <slzheng@alumni.cmu.edu>
2023-11-06 07:45:34 +0000

    add shapefile viewer

---
+++ view.py
@@ -0,0 +1,35 @@
+import click
+
+import geopandas as gpd
+from descartes import PolygonPatch
+import matplotlib.pyplot as plt
+
+import utils.visualization as viz
+
+
+@click.command()
+@click.option(
+    '--file',
+    default="data/temp/combined.shp",
+    type=str,
+    help='Shapefile to view',
+)
+def cli(file):
+    gdf = gpd.read_file(file)
+
+    polygons = []
+    labels = []
+    for shape, label in zip(gdf.geometry, gdf['label']):
+        if shape.geom_type == "MultiPolygon":
+            shapes = shape.geoms
+            polygons.extend(shapes)
+            labels.extend([label] * len(shapes))
+        else:
+            polygons.append(shape)
+            labels.append(label)
+
+    viz.show_polygons(polygons, labels)
+
+
+if __name__ == '__main__':
+    cli()
