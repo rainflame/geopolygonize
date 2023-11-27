13eb3cb40ee34a31fc7fdace8bd7be071e9de304
slz4025 <slzheng@alumni.cmu.edu>
2023-11-27 03:40:27 +0000

    variable input file to e2e

--- tests/run_e2e_test.sh
+++ tests/run_e2e_test.sh
@@ -3,9 +3,12 @@
 set -e
 set -x
 
+NAME="tiny"
+
 source .venv/bin/activate
-rm -f data/output.shp
-python3 tests/view.py --tiffile data/input.tif
-python3 -m src.geopolygonize --input-file="data/input.tif" --output-file="data/output.shp"
-python3 tests/view.py --shapefile data/output.shp
+
+rm -f data/$NAME.shp
+python3 tests/view.py --tiffile data/$NAME.tif
+python3 -m src.geopolygonize --input-file="data/$NAME.tif" --output-file="data/$NAME.shp"
+python3 tests/view.py --shapefile data/$NAME.shp
 deactivate
