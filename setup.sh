0203c65e2b5a215ed3e1f900d951200bad8f4dce
slz4025 <slzheng@alumni.cmu.edu>
2023-11-30 03:44:53 +0000

    README: simplify and add docs

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 git config --local core.hooksPath "$SCRIPT_DIR"/.githooks
--- tests/run_e2e_test.sh
+++ tests/run_e2e_test.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 set -x
 
-NAME="tiny"
+NAME=$1
 
 source .venv/bin/activate
-
-rm -f data/$NAME.shp
-python3 tests/view.py --tiffile data/$NAME.tif
+rm -f data/"$NAME".shp
+python3 tests/view.py --tiffile data/"$NAME".tif
 python3 -m src.geopolygonize --input-file="data/$NAME.tif" --output-file="data/$NAME.shp"
-python3 tests/view.py --shapefile data/$NAME.shp
+python3 tests/view.py --shapefile "data/$NAME.shp"
 deactivate
