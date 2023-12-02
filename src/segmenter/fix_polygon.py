a7466c779f2dac0b5d0251d9784585527766995b
slz4025 <slzheng@alumni.cmu.edu>
2023-12-02 21:27:37 +0000

    handle interiors + tests

--- src/segmenter/fix_polygon.py
+++ src/segmenter/fix_polygon.py
@@ -1,12 +1,33 @@
 from typing import List
 
-from shapely import Polygon
+from shapely import LineString, Polygon
 from shapely.ops import polygonize, unary_union
 
 
 def fix_polygon(polygon: Polygon) -> List[Polygon]:
     # https://gis.stackexchange.com/questions/423351/identifying-self-intersections-in-linestring-using-shapely
     exterior = polygon.exterior
-    polygons = list(polygonize(unary_union(exterior)))
-    # TODO: Figure out how to deal with interior boundaries if necessary.
-    return polygons
+    exterior_polygons = list(polygonize(unary_union(exterior)))
+    fixed_polygon_exterior: List[LineString] = [
+        ep.exterior for ep in exterior_polygons
+    ]
+
+    interior_polygons = [
+        p
+        for interior in polygon.interiors
+        for p in list(polygonize(unary_union(interior)))
+    ]
+    fixed_polygon_interiors: List[List[LineString]] = [
+        [] for i in range(len(exterior_polygons))
+    ]
+    for ip in interior_polygons:
+        for e, ep in enumerate(exterior_polygons):
+            if ep.contains(ip):
+                fixed_polygon_interiors[e].append(ip.exterior)
+                break
+
+    fixed_polygons = []
+    for fe, fis in list(zip(fixed_polygon_exterior, fixed_polygon_interiors)):
+        fixed_polygon = Polygon(fe, fis)
+        fixed_polygons.append(fixed_polygon)
+    return fixed_polygons
