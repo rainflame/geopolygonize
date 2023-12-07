2927b74b4e19a9e9723990c52cd496f833f32ef7
slz4025 <slzheng@alumni.cmu.edu>
2023-12-07 02:48:57 +0000

    gpkg

--- tests/run_e2e_test.sh
+++ tests/run_e2e_test.sh
@@ -6,8 +6,8 @@ set -x
 NAME=$1
 
 source .venv/bin/activate
-rm -f data/"$NAME".shp
+rm -f data/"$NAME".gpkg
 python3 tests/view.py --tiffile data/"$NAME".tif
-python3 -m src.geopolygonize --input-file="data/$NAME.tif" --output-file="data/$NAME.shp"
-python3 tests/view.py --shapefile "data/$NAME.shp"
+python3 -m src.geopolygonize --input-file="data/$NAME.tif" --output-file="data/$NAME.gpkg"
+python3 tests/view.py --gpkgfile "data/$NAME.gpkg"
 deactivate
